4b34e79215968b73c3057377ea3a985e8bda6e3d
Harold Lomotey <haroldlomotey@gmail.com>
2016-03-18 13:29:28 -0400

    Added files via upload

---
+++ HomeViewController.swift
@@ -0,0 +1,25 @@
+//
+//  HomeViewController.swift
+//  Serenity
+//
+//  Created by Harold  on 2/29/16.
+//  Copyright © 2016 Harold . All rights reserved.
+//
+
+import UIKit
+
+
+class HomeViewController: UIViewController {
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        navigationController?.navigationBarHidden = false    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+}
